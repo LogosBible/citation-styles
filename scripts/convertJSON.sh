02eb4cf1a90502c2c12c5e41cbe9f7f26651862b
Tom Philpot <tom.philpot@logos.com>
2014-08-07 15:52:55 -0700

    Added some checks to make the JSON generation script slightly more user friendly.

--- scripts/convertJSON.sh
+++ scripts/convertJSON.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+# Set this to your Code folder
+CODE_DIR="$HOME/Code50"
+
+
 SCRIPT_DIR=`dirname "$0"`
 MAKE_JSON="$SCRIPT_DIR/../scripts/makejson.py"
-OUTPUT_DIR="$1"
+DEFAULT_OUTPUT_DIR="$CODE_DIR/DigitalLibrary/src/Libronix.DigitalLibrary/CitationStyles/"
+OUTPUT_DIR=${1:-$DEFAULT_OUTPUT_DIR}
+
+if [[ -z $OUTPUT_DIR || ! -d $OUTPUT_DIR ]]
+then
+	echo "Please specify a folder to copy the .json output files to."
+	echo "e.g. $OUTPUT_DIR"
+	exit 1;
+fi
 
 pushd "$SCRIPT_DIR/.."
 
-for STYLE in apa bibtex chicago-fullnote-bibliography din-1505-2 harvard1 modern-language-association refer-bibix ris society-of-biblical-literature-fullnote-bibliography turabian-fullnote-bibliography american-anthropological-association modern-humanities-research-association american-political-science-association american-sociological-association unified-style-linguistics pontifical-biblical-institute pontifical-athenaeum-regina-apostolorum
+for STYLE in apa bibtex chicago-fullnote-bibliography din-1505-2 harvard1 modern-language-association refer-bibix ris society-of-biblical-literature-fullnote-bibliography turabian-fullnote-bibliography american-anthropological-association modern-humanities-research-association american-political-science-association american-sociological-association unified-style-linguistics pontifical-biblical-institute pontifical-athenaeum-regina-apostolorum christian-writers-manual-of-style
 do
     echo "Converting $STYLE from CSL to JSON"
     python "$MAKE_JSON" $STYLE.csl > $STYLE.json
